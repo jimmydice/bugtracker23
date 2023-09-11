20f719d25abab6c5de5a3044fa2e9e92c14ce88f
jimmydice <jimmydice@gmail.com>
2023-09-11 11:42:20 +0300

    strong password

--- website/password.py
+++ website/password.py
@@ -1,10 +1,22 @@
 import re
 
-# Regular expression to match at least one special character
+# Match at least one special character
 special_char_pattern = r'[!@#$%^&*(),.?":{}|<>]'
 
 # Function to validate the password
 def is_valid_password(password):
+    """
+    Validates a password based on the following criteria:
+    
+    1. The password must be at least 6 characters long.
+    2. The password must contain at least one special character.
+    
+    Args:
+        password (str): The password to be validated.
+
+    Returns:
+        bool: True if the password meets the criteria, False otherwise.
+    """
     # Check if the password is at least 6 characters long
     if len(password) < 6:
         return False
